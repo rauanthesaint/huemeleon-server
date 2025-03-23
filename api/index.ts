bed289b39b340655094b002d1c581337f0f461ad
Rauan Karanov <rauanthesaint@gmail.com>
2025-03-23 16:49:06 +0500

    Fixed https://vercel.link/missing-public-directory 9.0

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,6 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import { PORT } from "./config";
 import router from "./router";
-import { VercelRequest, VercelResponse } from "@vercel/node";
-import { createServer } from "http";
 
 const app = express();
 
@@ -24,8 +22,4 @@ app.get("/", (request, response) => {
 app.listen(PORT, () =>
   console.log("\x1b[32m%s\x1b[0m", `Server is ready on port ${PORT}`)
 );
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const server = createServer(app);
-  server.emit("request", req, res);
-}
+export default app;
